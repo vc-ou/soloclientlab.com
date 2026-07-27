4eb3063de126dfd425da5e59ace77d268fd58792
vc <vc@users.noreply.github.com>
2026-07-27 12:54:05 +0800

    Prevent admin pages from indexing

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,13 @@
+import type { Metadata } from "next";
+
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false
+  }
+};
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return children;
